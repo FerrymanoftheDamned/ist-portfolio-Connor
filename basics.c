a2ea9a4d1b675e353e8134f6a666c8e5995e67c2
Ferryman of the Damned <2027fleet.connor@fcboe.org>
2024-02-08 13:09:35 -0500

    idk

---
+++ basics.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("this file includes C basics\n");
+
+	int num;
+	printf("why is the what when the where, give number: ");
+	scanf("%i", &num);
+	printf("when the what with the how and the why %i\n", num);
+
+	return 0;
+}
